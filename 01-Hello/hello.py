9e39e10857b11d66b6f11c3589bc2bb6ee377662
eevans01 <eevans01@ferrisisd.org>
2018-02-17 20:59:28 -0600

    Update hello.py
    Latest updates entered manually into Github.

--- 01-Hello/hello.py
+++ 01-Hello/hello.py
@@ -1,1 +1,8 @@
-salutation = "Hello"
+runagain = "Y"
+while runagain == "Y" or runagain == "y":
+    user_name = input("Please Enter Your Name and Press ENTER: ")
+    print("")
+    print("Hello" + " " + user_name)
+    runagain = input("Run Program Again? [Y]es or [N]o and Press ENTER")
+print("")
+print("Thank You")
