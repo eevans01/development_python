4fe6c7e876dbad890015b28cc89f16d0f5a6d697
eevans01 <eevans01@ferrisisd.org>
2018-02-18 19:27:46 -0600

    Initial Code

--- 02-WorkingWithVariables/variables.py
+++ 02-WorkingWithVariables/variables.py
@@ -1,1 +1,21 @@
+#Programmer Name: Your Name
+#Program Name: Working with Variables
+#Description: Uses variables to produce output.
 
+greeting = "Hello and Welcome"
+print (greeting)
+print ("")
+problem1 = "1 + 1 ="
+answer1 = 1 + 1
+print (problem1)
+print (answer1)
+print ("")
+problem2 = "2 + 2 ="
+answer2 = 2 + 2
+print (problem2)
+print (answer2)
+print ("")
+problem3 = "4 + 4 ="
+answer3 = 4 + 4
+print (problem3)
+print (answer3)
