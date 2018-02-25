4f1e7469f5d905a028cc4d1ae5f3b578ae1f464f
eevans01 <eevans01@ferrisisd.org>
2018-02-25 12:26:00 -0600

    Initial code load without comments.

--- 10-Matrix/matrix.py
+++ 10-Matrix/matrix.py
@@ -1,1 +1,60 @@
+w, h = 4, 11;
+a = [[0 for x in range(w)] for y in range(h)] 
+a[0][0] = "127"
+a[1][0] = "5443"
+a[2][0] = "9402"
+a[3][0] = "9403"
+a[4][0] = "10143"
+a[5][0] = "11085"
+a[6][0] = "11242"
+a[7][0] = "12645"
+a[8][0] = "12650"
+a[9][0] = "12992"
+a[10][0] = "13905"
 
+a[0][1] = "Ben Barber Innovation Academy"
+a[1][1] = "Harmony School of Innovation"
+a[2][1] = "Henderson Junior High School"
+a[3][1] = "Henderson Junior High School"
+a[4][1] = "Harmony School of Innovation"
+a[5][1] = "Mansfield High School"
+a[6][1] = "Ferris High School"
+a[7][1] = "Ferris High School"
+a[8][1] = "Ferris Junior High School"
+a[9][1] = "Italy High School"
+a[10][1] = "Faith Family Academy"
+
+a[0][2] = "Mansfield, TX"
+a[1][2] = "Ft. Worth, TX"
+a[2][2] = "Stephenville, TX"
+a[3][2] = "Stephenville, TX"
+a[4][2] = "Ft. Worth, TX"
+a[5][2] = "Mansfield, TX"
+a[6][2] = "Ferris, TX"
+a[7][2] = "Ferris, TX"
+a[8][2] = "Ferris, TX"
+a[9][2] = "Italy, TX"
+a[10][2] = "Waxahachie, TX"
+
+a[0][3] = "The Fighting Pickles"
+a[1][3] = "Synergy"
+a[2][3] = "Hive of Steel"
+a[3][3] = "CyberSwarm"
+a[4][3] = "Bits and Bots"
+a[5][3] = "Mad Hackers"
+a[6][3] = "ERROR 451"
+a[7][3] = "S.C.R.E.W. Ups"
+a[8][3] = "Cannot Compute"
+a[9][3] = "Vindicators"
+a[10][3] = "Eagles Robotics"
+print(a[0][0] + " - " + a[0][3] + " - " + a[0][1] + " - " + a[0][2])
+print(a[1][0] + " - " + a[1][3] + " - " + a[1][1] + " - " + a[1][2])
+print(a[2][0] + " - " + a[2][3] + " - " + a[2][1] + " - " + a[2][2])
+print(a[3][0] + " - " + a[3][3] + " - " + a[3][1] + " - " + a[3][2])
+print(a[4][0] + " - " + a[4][3] + " - " + a[4][1] + " - " + a[4][2])
+print(a[5][0] + " - " + a[5][3] + " - " + a[5][1] + " - " + a[5][2])
+print(a[6][0] + " - " + a[6][3] + " - " + a[6][1] + " - " + a[6][2])
+print(a[7][0] + " - " + a[7][3] + " - " + a[7][1] + " - " + a[7][2])
+print(a[8][0] + " - " + a[8][3] + " - " + a[8][1] + " - " + a[8][2])
+print(a[9][0] + " - " + a[9][3] + " - " + a[9][1] + " - " + a[9][2])
+print(a[10][0] + " - " + a[10][3] + " - " + a[10][1] + " - " + a[10][2])
